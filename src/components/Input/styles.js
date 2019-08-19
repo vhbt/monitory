c8cd424c89bf164d0119d85dd211f964c225125c
Vitor Hariel <vhbarauna@gmail.com>
2019-08-19 12:28:17 -0300

    style: input size

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,7 +3,8 @@ import colors from '../../constants/theme';
 
 export const TextComponent = styled.TextInput`
   color: ${colors.black};
-  height: 42px;
+  height: 36px;
+  padding-top: 10px;
   border-bottom-color: ${colors.gray2};
   border-bottom-width: 1px;
 `;
