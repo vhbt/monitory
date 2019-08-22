7a0105ed5497e614b5d9321fb6e722e4bd87ac68
Vitor Hariel <vhbarauna@gmail.com>
2019-08-21 22:43:20 -0300

    add: push notification

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {StatusBar} from 'react-native';
 import FlashMessage from 'react-native-flash-message';
+import OneSignal from 'react-native-onesignal';
 
 import './config/ReactotronConfig';
 
 import {store, persistor} from './store';
 import App from './App';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-        <FlashMessage position="top" />
-        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-      </PersistGate>
-    </Provider>
-  );
+export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    OneSignal.init('7c054be9-7863-4524-beff-5e6c36c0cd3b');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+          <FlashMessage position="top" />
+          <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
