755cfa1d2a5fb3d603b9345b3de4a407c58e1ac9
Vitor Hariel <vhbarauna@gmail.com>
2019-08-22 00:28:08 -0300

    fix: push notification tagging

--- src/store/modules/profile/saga.js
+++ src/store/modules/profile/saga.js
@@ -33,8 +33,9 @@ export function* login({payload}) {
     suap_api.defaults.headers.authorization = `JWT ${token}`;
     api.defaults.headers.authorization = `JWT ${token}`;
 
+    OneSignal.setExternalUserId(user.matricula);
+
     OneSignal.sendTags({
-      matricula: user.matricula,
       curso: user.curso,
       curso_ano: user.curso_ano,
       curso_turno: user.curso_turno,
@@ -68,9 +69,19 @@ export function* updateUser({payload}) {
       curso_turno,
     });
 
+    const user = response.data;
+
+    OneSignal.setExternalUserId(user.matricula);
+
+    OneSignal.sendTags({
+      curso: user.curso,
+      curso_ano: user.curso_ano,
+      curso_turno: user.curso_turno,
+    });
+
     showMessage({type: 'success', message: 'Dados atualizados com sucesso.'});
 
-    yield put(updateUserSuccess(response.data));
+    yield put(updateUserSuccess(user));
   } catch (err) {
     if (err.response) {
       showMessage({type: 'danger', message: err.response.data.detail});
@@ -89,6 +100,7 @@ export function* refresh() {
 
   const state = yield select();
   const {token} = state.profile;
+  const {user} = state.profile;
 
   if (!token) return;
 
@@ -103,13 +115,26 @@ export function* refresh() {
 
     suap_api.defaults.headers.authorization = `JWT ${token}`;
     api.defaults.headers.authorization = `JWT ${token}`;
+
+    OneSignal.setExternalUserId(user.matricula);
+
+    OneSignal.sendTags({
+      curso: user.curso,
+      curso_ano: user.curso_ano,
+      curso_turno: user.curso_turno,
+    });
   } catch (err) {
     yield put(logout());
   }
 }
 
+export function logOut() {
+  OneSignal.removeExternalUserId();
+}
+
 export default all([
   takeLatest('@profile/LOGIN_REQUEST', login),
   takeLatest('@profile/UPDATE_USER_REQUEST', updateUser),
+  takeLatest('@profile/LOGOUT', logOut),
   takeLatest('persist/REHYDRATE', refresh),
 ]);
